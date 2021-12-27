400be5a9d083f616139e724cc28d0276d0592605
chanyongee <rgblue0525@gmail.com>
2021-12-27 17:40:37 +0900

    keep-alive와 동적 컴포넌트

---
+++ dynamic_component/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>
+      <ul>
+        <li @click="changeMenu('Home')">Home</li>
+        <li @click="changeMenu('About')">About</li>
+        <li @click="changeMenu('Contact')">Contact</li>
+      </ul>
+    </div>
+
+    <div>
+      <keep-alive include="Home"> <!-- Home만 캐싱 -->
+        <component :is="currentView"></component>
+      </keep-alive>
+    </div>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue';
+import About from './components/About.vue';
+import Contact from './components/Contact.vue';
+
+export default {
+  components: { Home, About, Contact },
+  data() {
+    return { currentView : 'Home'}
+  }  ,
+  methods: {
+    changeMenu(view) {
+      this.currentView = view;
+    }
+  },
+}
+</script>
